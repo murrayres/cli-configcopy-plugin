cbd3233a5175512511febac9221c789db26eae7a
Murray Resinski <murray.resinski@octanner.com>
2019-09-10 15:45:05 -0600

    added redacted

--- index.js
+++ index.js
@@ -5,16 +5,22 @@
 async function ccopy(appkit, args) {
   try{
         var newlist = {}
+        var redactedlist = {}
         const configvars = await appkit.api.get('/apps/' + args.app + '/config-vars')
         var newlist = {}
         for (var key in configvars) {
 //TODO need to update to avoid others
             if (!key.includes("_VAULT_") && key != "DATABASE_URL" && key != "RABBITMQ_URL" && !key.startsWith("S3_") && key !="MONGODB_URL" && key !="REDIS_URL" && key != "ES_URL" && key != "KIBANA_URL" && key != "MEMCACHED_URL" && !configvars[key].includes("redacted")) {
                 newlist[key] = configvars[key]
+            }else{
+             redactedlist[key]=configvars[key]
             }
         }
        const resp = await appkit.api.patch(JSON.stringify(newlist), '/apps/' + args.t + '/config-vars'); 
        appkit.terminal.vtable(resp);
+       console.log("")
+       console.log("these variables not copied")
+       appkit.terminal.vtable(redactedlist);
   } catch (err) {
     appkit.terminal.error(err);
   }
